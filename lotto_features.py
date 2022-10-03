bbbf55ca7ca2db1e8a1e3347e78ba409bb3aa9c7
Sergulek35 <kaskov.S.P@yandex.ru>
2022-10-03 14:30:16 +0300

    magic

--- lotto_features.py
+++ lotto_features.py
@@ -5,11 +5,13 @@ def separator(simbol):  # Разделитель
     return simbol * 40
 
 
-
 class Kegs:
     def __init__(self):
         # количество бочонков в мешке
         self.kegs = [keg for keg in range(1, 91)]
+
+    def __str__(self):
+        return f'(Осталось - {len(self.kegs)})'
 
     def kegs_minus(self, number):
         # выкидываем случайный бочонок
@@ -21,7 +23,7 @@ class Card:
     def __init__(self):
         # создать карту
         selection = random.sample(range(1, 91), 15)
-        self.card = [*sorted(selection[:5]), *sorted(selection[5:10]), *sorted(selection[10:])]
+        self.card = [*sorted(selection[:5]), '\n', '', *sorted(selection[5:10]), '\n', *sorted(selection[10:])]
         self.once = 0
 
     def card_out(self, number):
@@ -32,10 +34,17 @@ class Card:
         self.card.remove(number)
         self.once += 1
 
+    def __eq__(self, other):
+        return self.card != self.once
+
 
 class Person:
     def __init__(self):
         self.person = {}
+
+    def __str__(self):
+        return f'Всего игроков: {len(self.person)}'
+
 
     def add_player(self, name):
         if name in self.person:
